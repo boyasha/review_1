f7d224bca780bc45ceac3ca849a8d1a805738d9f
boyasha <alexeybo17@yandex.ru>
2022-04-19 14:59:51 +0300

    checkpoint 2

--- Algorithms.py
+++ Algorithms.py
@@ -1,7 +1,12 @@
-from tkinter import messagebox
+from tkinter import *
+from tkinter import messagebox, Toplevel
 
 
-def caesar_encryption(way_to_file, shift):
+def error():
+    messagebox.showwarning(message="Что-то пошло не так, попробуйте заново!")
+
+
+def caesar_encryption(way_to_file, shift, flag = ""):
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     en_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -19,24 +24,31 @@ def caesar_encryption(way_to_file, shift):
         text_from_file = open(way_to_file, "r").read()
         for i in text_from_file:
             if i in en_alphabet:
-                if en_alphabet.index(i) < 25:
-                    result_string += en_alphabet[en_alphabet.index(i) + shift]
+                shift_en = shift % 26
+                if en_alphabet.index(i) < 26:
+                    result_string += en_alphabet[en_alphabet.index(i) + shift_en]
                 else:
-                    result_string += en_alphabet[en_alphabet.index(i) + shift]
+                    result_string += en_alphabet[en_alphabet.index(i) + shift_en]
             elif i in rus_alphabet:
+                shift_rus = shift % 33
                 if rus_alphabet.index(i) < 33:
-                    result_string += rus_alphabet[rus_alphabet.index(i) + shift]
+                    result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
                 else:
-                    result_string += rus_alphabet[rus_alphabet.index(i) + shift]
+                    result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
             elif i in numbers:
-                result_string += str((int(numbers[numbers.index(i)]) + shift) % 10)
+                shift_num = shift % 10
+                result_string += str((int(numbers[numbers.index(i)]) + shift_num) % 10)
             else:
                 result_string += i
+
+        if shift < 0:
+            result_string = result_string.swapcase()
 
         point_index = way_to_file.rfind('.')
         new_way_mod_file = way_to_file[:point_index]+'_modified'+way_to_file[point_index:]
         open(new_way_mod_file, "w+").write(result_string)
-        messagebox.showinfo('', f'Итог: {result_string} \n\n Путь к новому файлу: {new_way_mod_file}')
+
+        result_string += f"\n\n f{new_way_mod_file}"
 
     except Exception:
 
@@ -44,23 +56,29 @@ def caesar_encryption(way_to_file, shift):
             shift = int(shift)
             for i in way_to_file:
                 if i in en_alphabet:
-                    if en_alphabet.index(i) < 25:
-                        result_string += en_alphabet[en_alphabet.index(i) + shift]
+                    shift_en = shift % 26
+                    if en_alphabet.index(i) < 26:
+                        result_string += en_alphabet[en_alphabet.index(i) + shift_en]
                     else:
-                        result_string += en_alphabet[en_alphabet.index(i) + shift]
+                        result_string += en_alphabet[en_alphabet.index(i) + shift_en]
                 elif i in rus_alphabet:
+                    shift_rus = shift % 33
                     if rus_alphabet.index(i) < 33:
-                        result_string += rus_alphabet[rus_alphabet.index(i) + shift]
+                        result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
                     else:
-                        result_string += rus_alphabet[rus_alphabet.index(i) + shift]
+                        result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
                 elif i in numbers:
-                    result_string += str((int(numbers[numbers.index(i)]) + shift) % 10)
+                    shift_num = shift % 10
+                    result_string += str((int(numbers[numbers.index(i)]) + shift_num) % 10)
                 else:
                     result_string += i
 
-            messagebox.showinfo('', f'{result_string}')
+            if shift < 0:
+                result_string = result_string.swapcase()
 
-        except Exception:
-            messagebox.showwarning(message="Что-то пошло не так, попробуй заново!")
+        except Exception as exc:
+                error()
 
 
+def visiner_encryption(way_to_file, key, language, flag=""):
+    pass
--- Display.py
+++ Display.py
@@ -5,11 +5,18 @@ import Algorithms
 root = Tk()
 
 
-def showing_caesar_encryption():
-    root.title("Шифрование Цезаря")
+def destroy_useless_items():
+    for child in root.winfo_children():
+        if not isinstance(child, Menu):
+            child.destroy()
+
+
+def showing_caesar(name1, name2, flag=""):
+    destroy_useless_items()
+    root.title(f"{name1} Цезаря")
 
     input_way_label = StringVar()
-    input_way_label.set("Путь до файла:")
+    input_way_label.set("Путь до файла (Текст):")
     input_way = Entry(bd="4", width="40", textvariable=input_way_label)
     input_way.place(x=90, y=30)
 
@@ -26,8 +33,132 @@ def showing_caesar_encryption():
                              [input_way_label.set(""), shift_input_way_label.set("")])
     clear_input_way.place(x=335, y=65)
 
-    encrypt = Button(text="Зашифровать", activebackground="grey", width="38", height="2", command=lambda:
-                     Algorithms.caesar_encryption(input_way.get(), shift_input_way.get()))
+    encrypt = Button(text=name2, activebackground="grey", width="38", height="2", command=lambda:
+                     Algorithms.caesar_encryption(input_way.get(), flag + shift_input_way.get()))
+    encrypt.place(x=90, y=150)
+
+
+def showing_visener(name1, name2, flag=''):
+    destroy_useless_items()
+    root.title(f"{name1} Виженера")
+
+    input_way_label = StringVar()
+    input_way_label.set("Путь до файла (Текст):")
+    input_way = Entry(bd="4", width="40", textvariable=input_way_label)
+    input_way.place(x=90, y=30)
+
+    key_input_way_label = StringVar()
+    key_input_way_label.set("Ключ:")
+    key_input_way = Entry(bd="3", width="13", textvariable=key_input_way_label)
+    key_input_way.place(x=90, y=66)
+
+    choose_input_way = Button(text="Выбрать", activebackground="grey", command=lambda:
+                              input_way_label.set(filedialog.askopenfilename()))
+    choose_input_way.place(x=235, y=65)
+
+    clear_input_way = Button(text="Удалить", activebackground="grey", command=lambda:
+                             [input_way_label.set(""), key_input_way_label.set("")])
+    clear_input_way.place(x=335, y=65)
+
+    value_radiobutton = BooleanVar()
+    value_radiobutton.set(0)
+    rus_checkbutton = Radiobutton(text="Русский", variable=value_radiobutton, value=1)
+    en_checkbutton = Radiobutton(text="Английский", variable=value_radiobutton, value=0)
+    rus_checkbutton.place(x=83, y=110)
+    en_checkbutton.place(x=230, y=110)
+
+    encrypt = Button(text=name2, activebackground="grey", width="38", height="2",
+                     command=Algorithms.visiner_encryption(input_way.get(), key_input_way.get(), value_radiobutton.get()))
+    encrypt.place(x=90, y=150)
+
+
+def showing_vernam(name1, name2):
+    destroy_useless_items()
+    root.title(f"{name1} Вернама")
+
+    input_way_label = StringVar()
+    input_way_label.set("Путь до файла (Текст):")
+    input_way = Entry(bd="4", width="40", textvariable=input_way_label)
+    input_way.place(x=90, y=30)
+
+    key_input_way_label = StringVar()
+    key_input_way_label.set("Ключ:")
+    key_input_way = Entry(bd="3", width="13", textvariable=key_input_way_label)
+    key_input_way.place(x=90, y=66)
+
+    choose_input_way = Button(text="Выбрать", activebackground="grey", command=lambda:
+                              input_way_label.set(filedialog.askopenfilename()))
+    choose_input_way.place(x=235, y=65)
+
+    clear_input_way = Button(text="Удалить", activebackground="grey", command=lambda:
+                             [input_way_label.set(""), key_input_way_label.set("")])
+    clear_input_way.place(x=335, y=65)
+
+    value_radiobutton = BooleanVar()
+    value_radiobutton.set(0)
+    rus_checkbutton = Radiobutton(text="Русский", variable=value_radiobutton, value=1)
+    en_checkbutton = Radiobutton(text="Английский", variable=value_radiobutton, value=0)
+    rus_checkbutton.place(x=83, y=110)
+    en_checkbutton.place(x=230, y=110)
+
+    encrypt = Button(text=name2, activebackground="grey", width="38", height="2",
+                     command=Algorithms.visiner_encryption(input_way.get(), key_input_way.get(), value_radiobutton.get()))
+    encrypt.place(x=90, y=150)
+
+
+def showing_morse(name1, name2):
+    destroy_useless_items()
+    root.title(f"{name1} Азбуки Морзе")
+
+    input_way_label = StringVar()
+    input_way_label.set("Путь до файла (Текст):")
+    input_way = Entry(bd="4", width="40", textvariable=input_way_label)
+    input_way.place(x=90, y=30)
+
+    choose_input_way = Button(text="Выбрать", activebackground="grey", command=lambda:
+                              input_way_label.set(filedialog.askopenfilename()))
+    choose_input_way.place(x=235, y=65)
+
+    clear_input_way = Button(text="Удалить", activebackground="grey", command=lambda:
+                             input_way_label.set(""))
+    clear_input_way.place(x=335, y=65)
+
+    value_radiobutton = BooleanVar()
+    value_radiobutton.set(0)
+    rus_checkbutton = Radiobutton(text="Русский", variable=value_radiobutton, value=1)
+    en_checkbutton = Radiobutton(text="Английский", variable=value_radiobutton, value=0)
+    rus_checkbutton.place(x=83, y=110)
+    en_checkbutton.place(x=230, y=110)
+
+    encrypt = Button(text=name2, activebackground="grey", width="38", height="2")
+    encrypt.place(x=90, y=150)
+
+
+def showing_morse_partial_analysis(name1, name2):
+    destroy_useless_items()
+    root.title(f"{name1} Морзе частотным анализом")
+
+    input_way_label = StringVar()
+    input_way_label.set("Путь до файла (Текст):")
+    input_way = Entry(bd="4", width="40", textvariable=input_way_label)
+    input_way.place(x=90, y=30)
+
+    choose_input_way = Button(text="Выбрать", activebackground="grey", command=lambda:
+                              input_way_label.set(filedialog.askopenfilename()))
+    choose_input_way.place(x=235, y=65)
+
+    clear_input_way = Button(text="Удалить", activebackground="grey", command=lambda:
+                             input_way_label.set(""))
+    clear_input_way.place(x=335, y=65)
+
+    value_radiobutton = BooleanVar()
+    value_radiobutton.set(0)
+    rus_checkbutton = Radiobutton(text="Русский", variable=value_radiobutton, value=1)
+    en_checkbutton = Radiobutton(text="Английский", variable=value_radiobutton, value=0)
+    rus_checkbutton.place(x=83, y=110)
+    en_checkbutton.place(x=230, y=110)
+
+    encrypt = Button(text=name2, activebackground="grey", width="38", height="2")
     encrypt.place(x=90, y=150)
 
 
@@ -43,28 +174,37 @@ class Display:
     main_menu.add_cascade(label="Алгоритмы", menu=algorithm_menu, activebackground="grey")
     main_menu.add_cascade(label="Стеганография", menu=steg_menu, activebackground="grey")
 
-    algorithm_menu.add_command(label="Шифрование Цезаря", command=showing_caesar_encryption, activebackground="grey")
-    algorithm_menu.add_command(label="Шифрование Виженера", command=showing_caesar_encryption, activebackground="grey")
-    algorithm_menu.add_command(label="Шифрование Вернама", command=showing_caesar_encryption, activebackground="grey")
-    algorithm_menu.add_command(label="Шифрование Азбуки Морзе", command=showing_caesar_encryption,
-                               activebackground="grey")
+    algorithm_menu.add_command(label="Шифрование Цезаря",
+                               command=lambda: showing_caesar("Шифрование", "Зашифровать"), activebackground="grey")
+    algorithm_menu.add_command(label="Шифрование Виженера", activebackground="grey",
+                               command=lambda: showing_visener('Шифрование', 'Зашифровать', ''))
+    algorithm_menu.add_command(label="Шифрование Вернама", activebackground="grey", command=lambda:
+                               showing_vernam("Шифрование", "Зашифровать"))
+    algorithm_menu.add_command(label="Шифрование Азбуки Морзе", activebackground="grey", command=lambda:
+                               showing_morse("Шифрование", "Зашифровать"))
     algorithm_menu.add_separator()
-    algorithm_menu.add_command(label="Дешифрование Цезаря", command=showing_caesar_encryption, activebackground="grey")
-    algorithm_menu.add_command(label="Дешифрование Виженера", command=showing_caesar_encryption,
+    algorithm_menu.add_command(label="Дешифрование Цезаря",
+                               command=lambda: showing_caesar("Дешифрование", "Дешифровать", "-"),
                                activebackground="grey")
-    algorithm_menu.add_command(label="Дешифрование Вернама", command=showing_caesar_encryption, activebackground="grey")
-    algorithm_menu.add_command(label="Дешифрование Цезаря частотным анализом", command=showing_caesar_encryption,
-                               activebackground="grey")
-    algorithm_menu.add_command(label="Дешифрование Азбуки Морзе", command=showing_caesar_encryption,
-                               activebackground="grey")
+    algorithm_menu.add_command(label="Дешифрование Виженера",
+                               activebackground="grey",
+                               command=lambda: showing_visener('Дешифрование', 'Дешифровать', ''))
+    algorithm_menu.add_command(label="Дешифрование Вернама", activebackground="grey", command=lambda:
+                               showing_vernam("Дешифрование", "Дешифровать"))
+    algorithm_menu.add_command(label="Дешифрование Цезаря частотным анализом",
+                               activebackground="grey",
+                               command=lambda: showing_morse_partial_analysis("Дешифрование", "Дешифровать"))
+    algorithm_menu.add_command(label="Дешифрование Азбуки Морзе",
+                               activebackground="grey",
+                               command=lambda: showing_morse("Дешифрование", "Дешифровать"))
 
-    steg_menu.add_command(label="Внедрение в bmp", command=showing_caesar_encryption, activebackground="grey")
-    steg_menu.add_command(label="Внедрение в jpg", command=showing_caesar_encryption, activebackground="grey")
-    steg_menu.add_command(label="Внедрение в png", command=showing_caesar_encryption, activebackground="grey")
+    steg_menu.add_command(label="Внедрение в bmp", activebackground="grey")
+    steg_menu.add_command(label="Внедрение в jpg", activebackground="grey")
+    steg_menu.add_command(label="Внедрение в png", activebackground="grey")
     steg_menu.add_separator()
-    steg_menu.add_command(label="Извлечение из bmp", command=showing_caesar_encryption, activebackground="grey")
-    steg_menu.add_command(label="Извлечение из jpg", command=showing_caesar_encryption, activebackground="grey")
-    steg_menu.add_command(label="Извлечение из png", command=showing_caesar_encryption, activebackground="grey")
+    steg_menu.add_command(label="Извлечение из bmp", activebackground="grey")
+    steg_menu.add_command(label="Извлечение из jpg", activebackground="grey")
+    steg_menu.add_command(label="Извлечение из png", activebackground="grey")
 
     root.config(menu=main_menu)
     root.mainloop()
