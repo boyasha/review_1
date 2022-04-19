e3c881fc88b59686d6d5f00fd406b8469be77d9f
boyasha <alexeybo17@yandex.ru>
2022-04-19 23:48:05 +0300

    finished

--- Algorithms.py
+++ Algorithms.py
@@ -13,16 +13,18 @@ def get_new_way_to_mod_file(way_to_file, result_string):
 
 
 def caesar_encryption(way_to_file, shift, flag=""):
+    input_window = Toplevel()
+
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     en_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    EN_alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    rus_alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
-                    'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
-                    'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
-                    'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
 
+    rus_alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
+                    'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    RUS_alphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+                    'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
     result_string = ''
 
     try:
@@ -30,31 +32,33 @@ def caesar_encryption(way_to_file, shift, flag=""):
         text_from_file = open(way_to_file, "r").read()
         for i in text_from_file:
             if i in en_alphabet:
-                shift_en = shift % 26
-                if en_alphabet.index(i) < 26:
-                    result_string += en_alphabet[en_alphabet.index(i) + shift_en]
-                else:
-                    result_string += en_alphabet[en_alphabet.index(i) + shift_en]
+                result_string += en_alphabet[(en_alphabet.index(i) + shift) % len(en_alphabet)]
+            elif i in EN_alphabet:
+                result_string += EN_alphabet[(EN_alphabet.index(i) + shift) % len(EN_alphabet)]
             elif i in rus_alphabet:
-                shift_rus = shift % 33
-                if rus_alphabet.index(i) < 33:
-                    result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
-                else:
-                    result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
+                result_string += rus_alphabet[(rus_alphabet.index(i) + shift) % len(rus_alphabet)]
+            elif i in RUS_alphabet:
+                result_string += rus_alphabet[(rus_alphabet.index(i) + shift) % len(rus_alphabet)]
             elif i in numbers:
                 shift_num = shift % 10
                 result_string += str((int(numbers[numbers.index(i)]) + shift_num) % 10)
             else:
                 result_string += i
 
-        if shift < 0:
-            result_string = result_string.swapcase()
-
         point_index = way_to_file.rfind('.')
         new_way_mod_file = way_to_file[:point_index] + '_modified' + way_to_file[point_index:]
         open(new_way_mod_file, "w+").write(result_string)
 
-        result_string += f"\n\n f{new_way_mod_file}"
+        text_input_window = Label(input_window,
+                                  text=f"Итог: {result_string}\n\nПуть до зашифрованного файла: {new_way_mod_file}")
+        text_input_window.place(x=1, y=1)
+
+        copy_text_input_window = Button(input_window, text="Скопировать", activebackground="grey", command=lambda:
+        input_window.clipboard_append(result_string))
+        copy_text_input_window.place(x=1, y=70)
+
+        input_window.geometry("800x300")
+        input_window.mainloop()
 
     except Exception:
 
@@ -62,27 +66,32 @@ def caesar_encryption(way_to_file, shift, flag=""):
             shift = int(shift)
             for i in way_to_file:
                 if i in en_alphabet:
-                    shift_en = shift % 26
-                    if en_alphabet.index(i) < 26:
-                        result_string += en_alphabet[en_alphabet.index(i) + shift_en]
-                    else:
-                        result_string += en_alphabet[en_alphabet.index(i) + shift_en]
+                    result_string += en_alphabet[(en_alphabet.index(i)+shift) % len(en_alphabet)]
+                elif i in EN_alphabet:
+                    result_string += EN_alphabet[(EN_alphabet.index(i) + shift) % len(EN_alphabet)]
                 elif i in rus_alphabet:
-                    shift_rus = shift % 33
-                    if rus_alphabet.index(i) < 33:
-                        result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
-                    else:
-                        result_string += rus_alphabet[rus_alphabet.index(i) + shift_rus]
+                    result_string += rus_alphabet[(rus_alphabet.index(i) + shift) % len(rus_alphabet)]
+                elif i in RUS_alphabet:
+                    result_string += rus_alphabet[(rus_alphabet.index(i) + shift) % len(rus_alphabet)]
                 elif i in numbers:
                     shift_num = shift % 10
                     result_string += str((int(numbers[numbers.index(i)]) + shift_num) % 10)
                 else:
                     result_string += i
 
-            if shift < 0:
-                result_string = result_string.swapcase()
+                text_input_window = Label(input_window, text=f"Итог: {result_string}")
+                text_input_window.place(x=1, y=1)
 
-        except Exception:
+                copy_text_input_window = Button(input_window, text="Скопировать", activebackground="grey",
+                                                command=lambda:
+                                                input_window.clipboard_append(result_string))
+                copy_text_input_window.place(x=1, y=50)
+
+                input_window.geometry("800x300")
+                input_window.mainloop()
+
+        except Exception as exc:
+            print(exc)
             error()
 
 
@@ -275,7 +284,8 @@ def caesar_partial_analysis_encrtyption(way_to_file, language=0):
         shift = ord(list(count_letters.keys())[list(count_letters.values()).index(count_most_popular)]) - ord(
             popular_letter)
         for i in text_from_file:
-            if i == " ": continue
+            if i == " ":
+                continue
             result_string += chr(ord(i) - shift)
         point_index = way_to_file.rfind('.')
         new_way_mod_file = way_to_file[:point_index] + '_modified' + way_to_file[point_index:]
